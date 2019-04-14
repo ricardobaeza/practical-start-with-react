f9a99cb2c72bbd1afa44f3d591d7c5773ca96eae
Ricardo Baeza <baezaric3600@gmail.com>
2019-04-13 21:26:21 -0600

    finsihed the globomantics app

---
+++ src/main-page/featured-house.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import House from '../house'
+
+const FeaturedHouse = (props) => {
+    if (props.house) return (
+        <div>
+            
+            <House house={props.house} />
+        </div>
+        )
+    return (<div>No featured house at this time</div>)
+};
+
+export default FeaturedHouse;
